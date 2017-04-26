2dbffb80fa4353000b2acdd52dfc30da973bbeb4
JonCass <JCass1980@gmail.com>
2017-04-26 15:17:44 -0700

    Think 3001 perma busted

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -4,7 +4,34 @@ import moment from 'moment'
 import {addMessage} from '../api/messaging'
 
 const styles={
-
+    entry: {
+        display: 'flex'
+    },
+    roomContainer:{
+        position: 'relative',
+        height: '100%'
+    },
+    messagesContainer:{
+        position: 'absolute',
+        top:0,
+        bottom: 100,
+        overflow: 'auto',
+        width: '100%',
+        background: 'green'
+    },
+    inputContainer:{
+        position:'absolute',
+        textAlign:'center',
+        height:60,
+        width:'100%',
+        background:'#ddd',
+        top: 100
+    },
+    textEntry:{
+        width: '95%',
+        height: 40,
+        lineHeight: 42,
+    }
 }
 
 class Room extends Component {
