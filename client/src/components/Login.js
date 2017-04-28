dec69818f7e7bc63efff098f78791d6f42c30332
JonCass <JCass1980@gmail.com>
2017-04-28 11:47:19 -0700

    Save before changing user to object

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,47 +1,8 @@
 import React, {Component} from 'react'
 import {login} from '../api/messaging'
+import styles from './Login.styles.js'
 import 'font-awesome/css/font-awesome.css'
 
-const styles = {
-    avatarBox:{
-        fontSize: 22,
-        height: 50,
-        width: 300,
-        border: '1px solid grey',
-        borderRadius: 10,
-        display: 'inline-block',
-        marginLeft: 20,
-        lineHeight: '50px',
-        textIndent: 20,
-    },
-    name:{
-        width: 200,
-        height: 50,
-        fontSize: 18,
-        textIndent: 10,
-        margin: 20
-    },
-    icon:{
-        width: 320,
-        height: 50,
-        fontSize: 18,
-        textIndent: 10
-    },
-    color:{
-        height: 50,
-        border: 0
-    },
-    link:{
-        textDecoration: 'none',
-        color: 'black',
-        fontSize: 18,
-        width: 100,
-        height: 50,
-        display: 'inline-block',
-        border: '2px solid black',
-        marginLeft: 20
-    }
-}
 
 class Login extends Component {
     constructor() {
@@ -49,7 +10,7 @@ class Login extends Component {
         this.state = {
             username: '',
             icon: 'user',
-            iconColor: 'black'
+            iconColor: '#18FFFF'
         }
     }
     handleChange = (e) => {
@@ -68,9 +29,10 @@ class Login extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <input style={styles.name} placeholder='Enter Username' name='username' onChange={this.handleChange} />
                     <input style={styles.icon} placeholder='Enter Name of Font Awesome Icon' name='icon' onChange={this.handleChange} />
-                    <div style={styles.link}><a  href="http://fontawesome.io/icons/"> Icon List </a></div>
-                    <input style={styles.color} onChange={this.handleChange} name='iconColor' type='color' value={this.state.iconColor} />
-                    <div style={styles.avatarBox}>Current Avatar<i style={{color:this.state.iconColor}} className={'fa fa-'+this.state.icon} aria-hidden="true"></i></div>
+                    <button style={styles.link}><a style={styles.anchor} href="http://fontawesome.io/icons/"> Icon List </a></button>
+                    <div style={styles.avatarBox}>Current Avatar<i style={{color:this.state.iconColor,lineHeight: '50px',marginRight: 20,fontSize: 32}} className={'fa fa-'+this.state.icon} aria-hidden="true"></i></div>
+                    <div style={styles.colorBox}> Choose Color for Icon</div>
+                    <div style={styles.circle}><input style={styles.iconColor} onChange={this.handleChange} name='iconColor' type='color' value={this.state.iconColor} /></div>
                 </form>
             </div>
         )
---
+++ client/src/components/Login.styles.js
@@ -0,0 +1,73 @@
+const styles = {
+    avatarBox:{
+        fontSize: 22,
+        height: 50,
+        width: 300,
+        border: '1px solid grey',
+        borderRadius: 10,
+        marginLeft: 20,
+        lineHeight: '50px',
+        textIndent: 20,
+        display: 'inline-flex',
+        justifyContent: 'space-between'
+    },
+    colorBox:{
+        fontSize: 22,
+        height: 50,
+        width: 300,
+        border: '1px solid grey',
+        borderRadius: 10,
+        marginLeft: 20,
+        lineHeight: '50px',
+        textIndent: 20,
+        display: 'inline-block'
+    },
+    name:{
+        width: 200,
+        height: 50,
+        fontSize: 18,
+        textIndent: 10,
+        margin: 20
+    },
+    icon:{
+        width: 320,
+        height: 50,
+        fontSize: 18,
+        textIndent: 10
+    },
+    iconColor:{
+        height: 60,
+        width: 60,
+        position: 'relative',
+        top: -10,
+        right: 10
+    },
+    link:{
+        textDecoration: 'none',
+        color: 'black',
+        fontSize: 18,
+        width: 100,
+        height: 50,
+        border: '2px solid black',
+        marginLeft: 20,
+        borderRadius: 25,
+        background: 'blue'
+    },
+    anchor:{
+        textDecoration: 'none',
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    circle:{
+        width:40,
+        height:40,
+        borderRadius: 25,
+        overflow: 'hidden',
+        display: 'inline-block',
+        position: 'relative',
+        right: 50,
+        top: 12
+    }
+}
+
+export default styles
