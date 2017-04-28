29aa473238032e85c5dcd445316effd242b0c510
JonCass <JCass1980@gmail.com>
2017-04-28 10:57:14 -0700

    working on login page avatar selection

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,55 @@
 import React, {Component} from 'react'
 import {login} from '../api/messaging'
+import 'font-awesome/css/font-awesome.css'
+
+const styles = {
+    avatarBox:{
+        fontSize: 22,
+        height: 50,
+        width: 300,
+        border: '1px solid grey',
+        borderRadius: 10,
+        display: 'inline-block',
+        marginLeft: 20,
+        lineHeight: '50px',
+        textIndent: 20,
+    },
+    name:{
+        width: 200,
+        height: 50,
+        fontSize: 18,
+        textIndent: 10,
+        margin: 20
+    },
+    icon:{
+        width: 320,
+        height: 50,
+        fontSize: 18,
+        textIndent: 10
+    },
+    color:{
+        height: 50,
+        border: 0
+    },
+    link:{
+        textDecoration: 'none',
+        color: 'black',
+        fontSize: 18,
+        width: 100,
+        height: 50,
+        display: 'inline-block',
+        border: '2px solid black',
+        marginLeft: 20
+    }
+}
 
 class Login extends Component {
     constructor() {
         super()
         this.state = {
-            username: ''
+            username: '',
+            icon: 'user',
+            iconColor: 'black'
         }
     }
     handleChange = (e) => {
@@ -22,7 +66,11 @@ class Login extends Component {
         return(
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input placeholder='Enter Username' name='username' onChange={this.handleChange} />
+                    <input style={styles.name} placeholder='Enter Username' name='username' onChange={this.handleChange} />
+                    <input style={styles.icon} placeholder='Enter Name of Font Awesome Icon' name='icon' onChange={this.handleChange} />
+                    <div style={styles.link}><a  href="http://fontawesome.io/icons/"> Icon List </a></div>
+                    <input style={styles.color} onChange={this.handleChange} name='iconColor' type='color' value={this.state.iconColor} />
+                    <div style={styles.avatarBox}>Current Avatar<i style={{color:this.state.iconColor}} className={'fa fa-'+this.state.icon} aria-hidden="true"></i></div>
                 </form>
             </div>
         )
--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,76 +1,10 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import moment from 'moment'
 import {addMessage} from '../api/messaging'
+import styles from './Room.styles'
 
-const styles = {
-    avatar:{
-        background: 'red',
-        borderRadius: 10,
-        width: 20,
-        height: 20,
-        color: 'white',
-        textAlign: 'center',
-        lineHeight: '22px',
-        marginRight: 10
-    },
-    entry: {
-        display:'flex',
-        border: '1px solid grey',
-        borderWidth: '0 0 1px 0'
-    },
-    room: {
-        position:'relative',
-        height:'100%'
-    },
-    messagesContainer: {
-        position:'absolute',
-        top:0,
-        bottom:100,
-        overflow:'auto',
-        width:'95%',
-    },
-    inputContainer: {
-        position:'absolute',
-        textAlign:'center',
-        bottom:0,
-        height:60,
-        width:'100%',
-        border: '2px solid red',
-        borderRadius: 5,
-        background: 'black',
-        padding: 10
-    },
-    textEntry: {
-        width:'85%',
-        height:30,
-        lineHeight:'30px',
-        fontSize:16,
-        outline:0,
-        border: '2px solid #777',
-    },
-    timeName:{
-        display: 'inline-block'
-    },
-    name:{
-        marginRight: 10
-    },
-    time:{
-       position: 'absolute',
-       right: 0,
-       display: 'inline-block',
-       lineHeight: '26px',
-    },
-    sizeEntry:{
-        height:30,
-    },
-    fontEntry:{
-        height:30,
-    },
-    colorEntry:{
-        height:30,
-    }
-}
 class Room extends Component {
     constructor() {
         super()
